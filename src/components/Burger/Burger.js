741f95b83a5140141c08da1132676f25c3079de0
const-variable <maulikchaudhary5@gmail.com>
2019-05-21 17:22:12 +0530

    added dynamic ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,14 +3,17 @@ import "./Burger.css";
 import BurgerIngredient from './BurgerIngredients/BurgerIngredients'
 
 const Burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_,i)=> {
+                return <BurgerIngredient key={igKey+i} type={igKey}/>;
+            });
+        });
     return(
         <div className="Burger">
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
-            <BurgerIngredient type="cheese"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
-
         </div>
     );
 };
